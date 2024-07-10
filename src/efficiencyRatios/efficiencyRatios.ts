68dd0f678bf33198d647fd9bf4d140a4f7cf876e
hhuziuk <huziukwork@gmail.com>
2024-07-10 20:33:35 +0200

    updated efficiencyRatio.ts, updated readme.md

--- src/efficiencyRatios/efficiencyRatios.ts
+++ src/efficiencyRatios/efficiencyRatios.ts
@@ -2,14 +2,15 @@ import Big from 'big.js';
 import { LiveseyFinanceError } from '../errors/errors';
 
 // EPS (ttm)
-export function calculateEPS(netIncome: number, sharesOutstanding: number): number {
+export function calculateEPS(netIncome: number, preferredDividends: number, sharesOutstanding: number): number {
     if (sharesOutstanding === 0) {
         throw LiveseyFinanceError.ZeroDivisionError("Shares outstanding cannot be zero, as it would cause division by zero.");
     }
     const netIncomeBig = new Big(netIncome);
+    const preferredDividendsBig = new Big(preferredDividends);
     const sharesOutstandingBig = new Big(sharesOutstanding);
 
-    const eps = netIncomeBig.div(sharesOutstandingBig);
+    const eps = netIncomeBig.minus(preferredDividendsBig).div(sharesOutstandingBig);
     return parseFloat(eps.toString());
 }
 
